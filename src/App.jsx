fccb141c10f64fbf8e2862d9f1e24eb13bdaa695
OlafGFG <olafgfg1@gmail.com>
2024-06-15 22:33:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+import SignUp from './SignUp'
+import PhoneNumber from './pages/PhoneNumber'
+import Otp from './pages/Otp'
+import LandingPage from './pages/LandingPage'
+import Vendor from './pages/Vendor'
+import { Route,Routes } from 'react-router-dom'
+import User from './pages/User'
+import Filters from './components/Filters'
+import ItemPopUp from './components/ItemPopUp'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div className=' w-mwidth h-screen flex-col justify-center'>
+      <Routes>
+      <Route path='/' element={<SignUp/>}></Route>
+      <Route path='/email' element={<PhoneNumber/>}></Route>
+      <Route path='/otp' element={<Otp/>}></Route>
+      <Route path='/vendor' element={<Vendor/>}></Route>
+      <Route path='/landingpage' element={<LandingPage/>}></Route>
+      <Route path='/filters' element={<Filters/>}></Route>
+      <Route path='/itempopup' element={<ItemPopUp/>}></Route>
+      <Route path='/user' element={<User/>}></Route>
+      </Routes>
+      
+      </div>
+  )
+}
+
+export default App
